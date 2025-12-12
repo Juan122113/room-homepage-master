4a62cb294802102e25e961f29586b0eec1fdc703
Juan122113 <ammiel89@hotmail.com>
2025-12-12 19:30:59 -0300

    Add JS controls and responsive hero image updates
    Added data-js attributes to navigation control icons in index.html and selected them in main.js for future interactivity. Updated CSS to use mobile hero image by default and switch to desktop image on wider screens, improving responsiveness. Minor style adjustments for navigation and layout in desktop view.

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 const iconHamburger = document.querySelector('[data-js="icon-hamburger"]');
 const mainLinks = document.querySelector('[data-js="main-links"]');
 const iconClose = document.querySelector('[data-js="icon-close"]');
+const iconAngleLeft = document.querySelector('[data-js="icon-angle-left"]');
+const iconAngleRight = document.querySelector('[data-js="icon-angle-right"]');
+const width = document.documentElement.clientWidth;
+
+const navImagesUrls = ["../images/desktop-image-hero-1.jpg", "../images/desktop-image-hero-1.jpg"];
+console.log(width);
 
 iconHamburger.addEventListener('click', (e) => {
     mainLinks.style.display = 'flex';
-})
+});
 
 iconClose.addEventListener('click', (e) => {
     mainLinks.style.display = 'none';
-})
+});
+
+window.addEventListener("resize", () => {
+    console.log(width);
+});
