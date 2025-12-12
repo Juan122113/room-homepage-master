3aedfc55056148af34103e0a5a461d12e8bbcd17
Juan122113 <ammiel89@hotmail.com>
2025-12-12 19:42:52 -0300

    Add responsive hero image arrays and main hero selector
    Introduced separate arrays for desktop and mobile hero images in main.js and added a data-js attribute to the main hero section in index.html. Also updated the resize event to log window width and prepared for responsive navigation image handling.

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 const iconHamburger = document.querySelector('[data-js="icon-hamburger"]');
 const mainLinks = document.querySelector('[data-js="main-links"]');
+const mainHero = document.querySelector('[data-js="main-hero"]');
 const iconClose = document.querySelector('[data-js="icon-close"]');
 const iconAngleLeft = document.querySelector('[data-js="icon-angle-left"]');
 const iconAngleRight = document.querySelector('[data-js="icon-angle-right"]');
 const width = document.documentElement.clientWidth;
 
-const navImagesUrls = ["../images/desktop-image-hero-1.jpg", "../images/desktop-image-hero-1.jpg"];
-console.log(width);
+const navDeskImagesUrls = ["../images/desktop-image-hero-1.jpg", "../images/desktop-image-hero-2.jpg", "../images/desktop-image-hero-3.jpg"];
+const navMobImagesUrls = ["../images/mobile-image-hero-1.jpg", "../images/mobile-image-hero-2.jpg", "../images/mobile-image-hero-3.jpg"];
+// console.log(width);
 
 iconHamburger.addEventListener('click', (e) => {
     mainLinks.style.display = 'flex';
@@ -17,5 +19,11 @@ iconClose.addEventListener('click', (e) => {
 });
 
 window.addEventListener("resize", () => {
-    console.log(width);
+    console.log(window.innerWidth);
+
+    if (window.innerWidth >= 768 ) {
+        iconAngleRight.addEventListener("click", () => {
+
+        });
+    }
 });
