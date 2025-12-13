c6df46a794fccc644064bc6d9a92fc5c866985e0
Juan122113 <ammiel89@hotmail.com>
2025-12-13 19:42:49 -0300

    Enable mobile hero image change on resize
    Uncommented and updated the resize event listener to allow changing the hero image to the mobile version when the window width is 768px or less and the right angle icon is clicked. Added console logs for debugging current width and resize events.

--- main.js
+++ main.js
@@ -4,9 +4,11 @@ const mainHero = document.querySelector('[data-js="main-hero"]');
 const iconClose = document.querySelector('[data-js="icon-close"]');
 const iconAngleLeft = document.querySelector('[data-js="icon-angle-left"]');
 const iconAngleRight = document.querySelector('[data-js="icon-angle-right"]');
-const width = document.documentElement.clientWidth;
+// const width = document.documentElement.clientWidth;
 
 let currentWidth = document.documentElement.clientWidth;
+
+console.log(currentWidth);
 
 const navDeskImagesUrls = ["url('../images/desktop-image-hero-1.jpg')", "url('../images/desktop-image-hero-2.jpg')", "../images/desktop-image-hero-3.jpg"];
 const navMobImagesUrls = ["../images/mobile-image-hero-1.jpg", "url('../images/mobile-image-hero-2.jpg')", "../images/mobile-image-hero-3.jpg"];
@@ -37,22 +39,24 @@ iconClose.addEventListener('click', (e) => {
 window.addEventListener("resize", () => {
     console.log(window.innerWidth);
 
-    currentWidth = window.innerWidth;
+    // currentWidth = window.innerWidth;
 
-    // if (window.innerWidth <= 768 ) {
+    if (window.innerWidth <= 768 ) {
 
-    //     console.log("less 768");
+        console.log("less 768");
+        // console.log(window.innerWidth);
         
-    //     iconAngleRight.addEventListener('click', () => {
+        iconAngleRight.addEventListener('click', () => {
 
-    //         console.log("click iconAngleRight");
-    //         // mainHero.style.backgroundImage = navMobImagesUrls[1];
-    //         mainHero.style.backgroundImage = "url('../images/mobile-image-hero-2.jpg')";
-    //     });
-    // }
+            console.log("click iconAngleRight");
+            // mainHero.style.backgroundImage = navMobImagesUrls[1];
+            mainHero.style.backgroundImage = "url('../images/mobile-image-hero-2.jpg')";
+        });
+    }
 });
 // });
 // }
+console.log(currentWidth);
 
 if (currentWidth <= 768) {
 
